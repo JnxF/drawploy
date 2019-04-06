fcc1477b350857c4697331d3e0b09151b69a7e75
Oriol Closa <oriol.closa@est.fib.upc.edu>
2019-04-06 18:29:47 +0200

    Fix gunicorn app

--- backend/server.sh
+++ backend/server.sh
@@ -2,4 +2,4 @@
 
 export PROD_MODE="True"
 
-./env/bin/gunicorn --workers 1 --log-file=gunicorn.log --bind unix:drawploy.sock website.wsgi:application
+./env/bin/gunicorn --workers 1 --log-file=gunicorn.log --bind unix:drawploy.sock backend.wsgi:application
