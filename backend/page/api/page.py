4f4bbfd1a182f2b46d011362510fa0a1561113b1
Joaquim De la Cruz <joaquimdcp@gmail.com>
2019-04-07 00:57:37 +0200

    Paco hot fix

--- backend/page/api/page.py
+++ backend/page/api/page.py
@@ -28,10 +28,10 @@ def find_type(label: str):
         "bd": 1
     }
 
-    centers = ["asia-east1", "asia-east2", "asia-northeast1", "asia-south1", "asia-southeast1", "australia-southeast1", "europe-north1", "europe-west1", "europe-west2", "europe-west3", "europe-west4", "europe-west6", "northamerica-northeast1", "southamerica-east1", "us-central1", "us-east1", "us-east4", "us-west1", "us-west2"]
+    # centers = ["asia-east1", "asia-east2", "asia-northeast1", "asia-south1", "asia-southeast1", "australia-southeast1", "europe-north1", "europe-west1", "europe-west2", "europe-west3", "europe-west4", "europe-west6", "northamerica-northeast1", "southamerica-east1", "us-central1", "us-east1", "us-east4", "us-west1", "us-west2"]
 
-    for c in centers:
-        values[c] = c
+    # for c in centers:
+    #     values[c] = c
 
     label = label.lower()
 
