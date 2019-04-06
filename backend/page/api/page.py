d3b2ec4137cc6394ee7485b64136ab9db70769b2
Joaquim De la Cruz <joaquimdcp@gmail.com>
2019-04-07 01:53:06 +0200

    Some dict fix

--- backend/page/api/page.py
+++ backend/page/api/page.py
@@ -26,13 +26,10 @@ import operator
 def find_type(label: str):
     values = {
         "vm": 0,
-        "bd": 1
+        "net": 2,
+        "bd": 1,
+
     }
-
-    # centers = ["asia-east1", "asia-east2", "asia-northeast1", "asia-south1", "asia-southeast1", "australia-southeast1", "europe-north1", "europe-west1", "europe-west2", "europe-west3", "europe-west4", "europe-west6", "northamerica-northeast1", "southamerica-east1", "us-central1", "us-east1", "us-east4", "us-west1", "us-west2"]
-
-    # for c in centers:
-    #     values[c] = c
 
     label = label.lower()
 
