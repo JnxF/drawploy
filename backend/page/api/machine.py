eef2bd3d74c8b04efc9cb8caf1e27158606bf32c
Joaquim De la Cruz <joaquimdcp@gmail.com>
2019-04-07 00:29:48 +0200

    AST done

---
+++ backend/page/api/machine.py
@@ -0,0 +1,26 @@
+
+class Square:
+    box = []
+    tipo = 0
+    i = ''
+
+    def __init__(self, i, tipo, box):
+        self.i = i
+        self.box = box
+        self.tipo = tipo
+    
+    def __lt__(self, other):
+        self.minX() < other.minX()
+
+    def minX(self):
+        return self.box[0]
+    
+    def maxX(self):
+        return self.box[2]
+    
+    def minY(self):
+        return self.box[1]
+
+    @classmethod
+    def solapaX(cls, a, b):
+        return ( (b.minX() >= a.minX() and b.minX() <= a.maxX()) or (a.minX() >= b.minX() and a.minX() <= b.maxX()) )
