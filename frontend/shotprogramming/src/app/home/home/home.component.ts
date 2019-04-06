3409a5eef866712389dabc59c7b169f336afb02f
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2019-04-06 15:39:59 +0200

    style

--- frontend/shotprogramming/src/app/home/home/home.component.ts
+++ frontend/shotprogramming/src/app/home/home/home.component.ts
@@ -5,7 +5,7 @@ import {ApiService} from "../../api/api.service";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
