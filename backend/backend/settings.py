512d86d56c9b8854b5bde06c8ce56dba1fa86f8a
Oriol Closa <oriol.closa@est.fib.upc.edu>
2019-04-07 00:13:02 +0200

    A

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'page'
 ]
 
 MIDDLEWARE = [
--- backend/page/api/providers/google.py
+++ backend/page/api/providers/google.py
@@ -8,7 +8,7 @@ from urllib3.connectionpool import xrange
 from backend.settings import GOOGLE_PROJECT
 
 url_template = "https://www.googleapis.com/deploymentmanager/v2/projects/" + GOOGLE_PROJECT + "/global/deployments"
-url_template_list = "https://www.googleapis.com/deploymentmanager/v2beta1/projects/" + GOOGLE_PROJECT + "/global/deployments"
+url_template_get = "https://www.googleapis.com/deploymentmanager/v2/projects/" + GOOGLE_PROJECT + "/global/deployments/"
 
 
 def _deploy(data: dict, token: str):
@@ -28,12 +28,26 @@ def _get_list(token: str):
         'Content-Type': "application/json",
     }
     response = requests.request("GET", url_template, headers=headers)
+    if response:
+        return json.loads(response.text)
+    return dict()
+
+
+def _get_deployment(id: str, token: str):
+    url_tmp = url_template_get + id
+    headers = {
+        'Authorization': "Bearer " + token,
+        'Content-Type': "application/json",
+    }
+    print(url_tmp)
+    response = requests.request("GET", url_tmp, headers=headers)
     return json.loads(response.text)
 
 
 def _create_content(data: dict, token: str):
+    name = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(26)]).lower()
     payload = {
-        "name": ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(26)]).lower(),
+        "name": name,
         "target": {
             "config": {
                 "content": data
@@ -46,4 +60,6 @@ def _create_content(data: dict, token: str):
         'Content-Type': "application/json",
     }
     response = requests.request("POST", url_template, data=payload, headers=headers)
+    response.text["targetName"] = name
+    response.text["targetTarget"] = data
     return json.loads(response.text)
