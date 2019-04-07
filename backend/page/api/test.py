7faa4c51caf466e3d043e81ff7a2eeec5b8403a8
Oriol Closa <oriol.closa@est.fib.upc.edu>
2019-04-07 04:27:08 +0200

    Distances

--- backend/page/api/test.py
+++ backend/page/api/test.py
@@ -1,19 +1,42 @@
-import numpy as np
-import cv2
+import math
+
+net = [
+  383,
+  668,
+  416,
+  287,
+  473,
+  296,
+  437,
+  669
+]
+
+vm = [
+  574,
+  267,
+  650,
+  264,
+  652,
+  318,
+  577,
+  321
+]
 
 
-im = cv2.imread('draw.jpeg', 0)
-thresh_img = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,3)
+def compute_centre(bounding):
+    x = bounding[0] + (bounding[0] - bounding[3])/2
+    y = bounding[1] + (bounding[1] - bounding[7])/2
+    return x, y
 
-_, contours, _ = cv2.findContours(thresh_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-result = []
-for cnt in contours:
-    approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
-    cv2.drawContours("res", [approx], -1, (0, 0, 255), 2, lineType=8)
-    if 3 < len(approx) < 15:
-        _, _, w, h = cv2.boundingRect(approx)
-        hull = cv2.convexHull(cnt)
-        approx2 = cv2.approxPolyDP(hull, 0.01 * cv2.arcLength(hull, True), True)
-        cv2.drawContours("res", [approx2], -1, (0, 255), 2, lineType=8)
-    result.append(approx2)
-return result
+
+cen1 = compute_centre(net)
+cen2 = compute_centre(vm)
+
+
+def compute_distance(obj1, obj2):
+    return math.sqrt(pow(obj2[0]-obj1[0], 2) + pow(obj2[1]-obj1[1], 2))
+
+
+dist = compute_distance(cen1, cen2)
+
+print(dist)
